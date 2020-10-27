12d961cfdd8be5e3ef06ce7c72410db408a1efd4
NYAMUSI <nyamusimercy61@gmail.com>
2020-10-27 22:35:36 +0300

    printf function

--- _printf.c
+++ _printf.c
@@ -1,19 +1,14 @@
 #include "holberton.h"
-
-
 /**
  *_printf - produces output according to a format
  *@format:  a character string composed of zero or more directives
  *Return: Integer
  */
- 
-
 int _printf(const char *format, ...)
 {
 va_list ap;
 unsigned int i;
 char *str;
-
 va_start(ap, format);
 i = vfprintf(stdout, format, ap);
 switch (format[i])
@@ -35,6 +30,26 @@ i = va_arg(ap, int);
 putchar(i);
 break;
 case 'i':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'u':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'o':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'x':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'X':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'p':
 i = va_arg(ap, int);
 putchar(i);
 break;
