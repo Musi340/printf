2816652eae624a5945d2c3628e60145caaf72370
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2020-10-27 18:00:06 +0000

    question 4 addition

--- _printf.c
+++ _printf.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
-
-
+/**
+ *_printf - produces output according to a format
+ *@format:  a character string composed of zero or more directives
+ *Return: Integer
+ */
 int _printf(const char *format, ...)
 {
 va_list ap;
 unsigned int i;
 char *str;
-
 va_start(ap, format);
 i = vfprintf(stdout, format, ap);
-
-while (format[i])
-{
 switch (format[i])
 {
 case 'c':
@@ -34,7 +33,22 @@ case 'i':
 i = va_arg(ap, int);
 putchar(i);
 break;
-}
+case 'u':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'o':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'x':
+i = va_arg(ap, int);
+putchar(i);
+break;
+case 'X':
+i = va_arg(ap, int);
+putchar(i);
+break;
 }
 va_end(ap);
 return (i);
